3a1b1840b1df76d00fd84f13f20c943899983bc6
safalPatil27 <158482783+safalPatil27@users.noreply.github.com>
2024-03-28 19:48:12 +0530

    Ramai up

---
+++ index.php
@@ -0,0 +1,146 @@
+<html>
+
+<head>
+  <title>
+    Index
+  </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style/style.css">
+  <script src="jquery-3.6.4.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+</head>
+
+<body>
+  <section>
+    <center>
+      <img src="images/logo1.png" alt="logo" id="logo">
+    </center>
+  </section>
+  <section id="Nav">
+    <nav class="navbar navbar-expand-sm fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav" id="list">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Academics</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Placements</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Results</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Enquiry</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </section>
+  <section id="corousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="images/c1.jpeg" alt="First slide" style="height:600px;">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="images/c2.jpeg" alt="Second slide" style="height:600px;">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="images/c1.jpeg" alt="Third slide" style="height:600px;">
+        </div>
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="images/c2.jpeg" alt="fourth slide" style="height:600px;">
+        </div>
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="images/c1.jpeg" alt="Fifth slide" style="height:600px;">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section>
+  <section id="marquee">
+    <a href="#">
+      <marquee behavior="" direction="right">Notifications</marquee>
+    </a>
+  </section>
+  <div class="abt">
+    <div class="heading">
+      <h1>About Us</h1>
+      <p>At Our College, we take immense pride in providing an empowering and inclusive learning environment exclusively
+      </p>
+    </div>
+    <div class="container1">
+      <section class="about">
+        <div class="about-img">
+          <img src="images/c1.jpeg" alt="c1">
+        </div>
+        <div class="about-content">
+          <h2>Our Vision</h2>
+          <p>for girls. Since our inception, we have been dedicated to nurturing the intellectual, personal, and social
+            growth of our students. With a commitment to excellence, we offer a diverse range of educational programs,
+            including B.Sc, B.Com, and B.A, ensuring that each young woman finds her path to success.
+            Our vision is to be a premier institution that fosters a generation of confident, compassionate, and
+            competent
+            women leaders who excel in various spheres of life. We strive to create an atmosphere where students can
+            unleash
+            their full potential and emerge as global citizens with a strong sense of responsibility towards society.
+          </p>
+          <a href="" class="read-more">Read-More</a>
+        </div>
+
+      </section>
+    </div>
+  </div>
+  <section id="c">
+   <div class="Ccontainer">
+    <div class="content-section">
+      <div class="card">
+        <img src="images/c1.jpeg" alt="">
+        <h2>B.SC</h2>
+        <p>B.Sc. in Computer Science, Chemistry, Physics, Zoology, Botany, Mathematics."</p>
+      </div>
+      <div class="card">
+        <img src="images/c2.jpeg" alt="">
+        <h2>B.SC</h2>
+        <p>B.Sc. in Computer Science, Chemistry, Physics, Zoology, Botany, Mathematics."</p>
+      </div>
+      <div class="card">
+        <img src="images/c3.jpeg" alt="">
+        <h2>B.SC</h2>
+        <p>B.Sc. in Computer Science, Chemistry, Physics, Zoology, Botany, Mathematics."</p>
+      </div>
+    </div>
+   </div>
+  </section>
+</body>
+
+</html>
